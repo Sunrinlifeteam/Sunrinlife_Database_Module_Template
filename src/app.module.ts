720529a685bc25696492cd4e5031f7ad68c68b75
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-07-18 13:13:51 +0900

    feat: add module, package for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database.module';
 import { HelloModule } from './hello/hello.module';
 
 @Module({
   imports: [
-    HelloModule,
+    DatabaseModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: [`.env`],
     }),
+    HelloModule,
   ],
   controllers: [],
   providers: [],
